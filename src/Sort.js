a37972bb3af0e8715e61ad91d858e97125b69409
Yasith Piyarathne <45797225+pandorian7@users.noreply.github.com>
2024-10-11 17:51:53 +0000

    bubble sort v1 visualize

--- src/Sort.js
+++ src/Sort.js
@@ -4,13 +4,19 @@ import { App, snapshot, done, clear } from "./sort-visualizer";
 
 import "./sort.css";
 
-function sort(array) {
-  // do cool stuff here
-
-  // call snapshot any time you do anything to the array
-  // it's okay if you call it with duplicate value array,
-  // it will deduplicate for you
-  snapshot(array);
+function sort(nums) {
+  let tmp;
+  for (let i=nums.length; i>=0; i--) {
+    for (let j=0; j<i-1; j++) {
+      if (nums[j]>nums[j+1]) {
+        tmp = nums[j];
+        nums[j] = nums[j+1];
+        nums[j+1] = tmp;
+        snapshot(nums);
+      }
+    }
+  }
+  snapshot(nums);
 }
 
 export default function SortComponent() {
